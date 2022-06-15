d68d909c0903153b6469d0b4de5df19398b4e90d
sir-mr-bean <kroucher.1019@gmail.com>
2022-06-16 08:19:57 +1000

    fix cache headers v2

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -56,7 +56,7 @@ export const Header = (props): JSX.Element => {
 
   useEffect(() => {
     trpcContext
-      .fetchQuery(["squarecategories"], {
+      .fetchQuery(["categories"], {
         staleTime: Infinity,
       })
       .then((result) => setNavigation(result));
