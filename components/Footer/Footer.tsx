b89d0ef09ed66e82b51e94b35960d7dd39bda0a7
sir-mr-bean <kroucher.1019@gmail.com>
2022-06-20 08:21:56 +1000

    add footer links

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
@@ -6,10 +7,20 @@ const Footer = () => {
       <div className="flex flex-col w-full bg-footer-backdrop bg-cover bg-center h-full justify-end items-stretch bg-blend-lighten bg-white bg-opacity-20">
         <div className="flex justify-between items-center">
           <div className="flex justify-center items-center text-white space-x-3 text-xs sm:text-lg px-4 pb-6">
-            <span>Contact</span>
-            <span>Shipping</span>
-            <span>Returns</span>
-            <span>Privacy Policy</span>
+            <Link href="/contact-us">
+              <span className="cursor-pointer hover:scale-105">Contact</span>
+            </Link>
+            <Link href="/contact-us">
+              <span className="cursor-pointer hover:scale-105">Shipping</span>
+            </Link>
+            <Link href="/contact-us">
+              <span className="cursor-pointer hover:scale-105">Returns</span>
+            </Link>
+            <Link href="/privacy-policy">
+              <span className="cursor-pointer hover:scale-105">
+                Privacy Policy
+              </span>
+            </Link>
           </div>
           <div className="flex  space-x-3 p-3 items-center justify-center">
             <div className="w-7 h-7 sm:w-20 sm:h-20">
