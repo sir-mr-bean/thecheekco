3df311d3ddd85ec3191175a2b534ca7fb3414227
sir-mr-bean <kroucher.1019@gmail.com>
2022-06-26 11:18:23 +1000

    fix marker on map

--- components/Checkout/Map/GoogleMaps.tsx
+++ components/Checkout/Map/GoogleMaps.tsx
@@ -1,4 +1,11 @@
-import React, { Component } from "react";
+import React, {
+  Component,
+  ReactElement,
+  ReactNode,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 const shopMarker = [
   {
@@ -8,11 +15,11 @@ const shopMarker = [
 ];
 
 const SimpleMap = ({ children }) => {
-  const ref = React.useRef<HTMLDivElement>(null);
-  const [map, setMap] = React.useState<google.maps.Map>();
+  const ref = useRef<HTMLDivElement>(null);
+  const [map, setMap] = useState<google.maps.Map>();
   const shop = { lat: -16.921913551261635, lng: 145.77625914833033 };
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (ref.current && !map) {
       setMap(
         new window.google.maps.Map(ref.current, {
@@ -20,33 +27,15 @@ const SimpleMap = ({ children }) => {
           zoom: 18,
         })
       );
-      // setTimeout(() => {
-      //   handleDrawMarkers();
-      // }, 2000);
     }
   }, [ref, map]);
-
-  // const handleDrawMarkers = () => {
-  //   shopMarker.forEach((marker) => {
-  //     new google.maps.Marker({
-  //       position: marker,
-  //       map: map,
-  //       icon: {
-  //         url: "https://thecheekco.vercel.app/images/logo.png",
-  //         scaledSize: new google.maps.Size(50, 50),
-  //       },
-  //     });
-  //   });
-  // };
 
   return (
     <>
       <div ref={ref} className="w-full h-40" />
-      {React.Children.map(children, (child) => {
+      {React.Children.map<ReactNode, ReactNode>(children, (child) => {
         if (React.isValidElement(child)) {
-          // set the map prop on the child component
-          // tslint:disable-next-line
-          return React.cloneElement(child as React.ReactElement<any>, { map });
+          return React.cloneElement(child, { map });
         }
       })}
     </>
