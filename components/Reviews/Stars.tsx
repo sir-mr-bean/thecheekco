9500a78630439adc8f88f3ff19abd61ad9279168
sir-mr-bean <kroucher.1019@gmail.com>
2022-06-30 17:30:35 +1000

    fix all natural products on front page

--- components/Reviews/Stars.tsx
+++ components/Reviews/Stars.tsx
@@ -13,13 +13,13 @@ const Stars = ({
   const stars: ReactElement[] = [];
   if (review?.rating) {
     for (let i = 0; i < parseInt(review.rating.toString()); i++) {
-      stars.push(<BsStarFill key={i} />);
+      stars.push(<BsStarFill color="#602d0d" key={i} />);
     }
     if (parseFloat(review.rating.toString()) % 1 !== 0) {
-      stars.push(<BsStarHalf key={stars.length} />);
+      stars.push(<BsStarHalf color="#602d0d" key={stars.length} />);
     }
     for (let i = stars.length; i < 5; i++) {
-      stars.push(<BsStar key={stars.length} />);
+      stars.push(<BsStar color="#602d0d" key={stars.length} />);
     }
   } else if (rating) {
     for (let i = 0; i < rating; i++) {
@@ -32,11 +32,11 @@ const Stars = ({
       stars.push(<BsStar key={stars.length} />);
     }
   } else {
-    stars.push(<BsStar key={0} />);
-    stars.push(<BsStar key={1} />);
-    stars.push(<BsStar key={2} />);
-    stars.push(<BsStar key={3} />);
-    stars.push(<BsStar key={4} />);
+    stars.push(<BsStar color="#602d0d" key={0} />);
+    stars.push(<BsStar color="#602d0d" key={1} />);
+    stars.push(<BsStar color="#602d0d" key={2} />);
+    stars.push(<BsStar color="#602d0d" key={3} />);
+    stars.push(<BsStar color="#602d0d" key={4} />);
   }
   return <div className="flex items-center justify-center">{stars}</div>;
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { RoughNotation } from "react-rough-notation";
 import { CartState } from "../../context/Cart/Context";
-import { Dispatch, useEffect, useRef } from "react";
+import { Dispatch, useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import {
@@ -36,11 +36,13 @@ import home_decor from "../../public/images/Homepage/home_decor.png";
 import shower from "../../public/images/Homepage/shower.png";
 import skin_care from "../../public/images/Homepage/skin_care.png";
 import bumlogo from "../../public/images/Homepage/bumlogo.png";
+import Stars from "@/components/Reviews/Stars";
 
 export default function Home(
   props: InferGetStaticPropsType<typeof getStaticProps>
 ) {
   const notationRef = useRef(null);
+  const [randomAllNatural, setRandomAllNatural] = useState<CatalogObject[]>([]);
   const strayCatRef = useRef() as React.MutableRefObject<HTMLInputElement>;
   const { inViewport } = useInViewport(notationRef);
   const { data: categoriesData } = trpc.useQuery(["all-categories"], {
@@ -63,17 +65,36 @@ export default function Home(
       productImage?: string;
     }>;
   } = CartState();
-
-  const allNaturalProducts = productsData?.filter(
-    (p) =>
-      p.itemData?.variations?.[0]?.customAttributeValues?.[
-        "Square:a1089928-7880-407e-93f3-08dfe506ac14"
-      ]?.booleanValue === true
+  const { data: productReviewsData } = trpc.useQuery(
+    [
+      "review.fetch-reviews",
+      {
+        productIds: productsData?.map(
+          (product) => product.id as string
+        ) as string[],
+      },
+    ],
+    {
+      enabled: !!productsData,
+    }
   );
+  console.log(productReviewsData);
 
-  console.log(strayCatRef);
+  useEffect(() => {
+    const allNaturalProducts = productsData?.filter(
+      (p) =>
+        p.itemData?.variations?.[0]?.customAttributeValues?.[
+          "Square:a1089928-7880-407e-93f3-08dfe506ac14"
+        ]?.booleanValue === true
+    );
+    if (allNaturalProducts) {
+      const randomProducts = allNaturalProducts
+        .sort(() => 0.5 - Math.random())
+        .slice(0, 6);
+      setRandomAllNatural(randomProducts);
+    }
+  }, [productsData]);
 
-  //create a typesafe function to return an icon from the react-icons library based on the icon name
   const Icon = (iconName: string) => {
     switch (iconName) {
       case "FaKissWinkHeart":
@@ -473,13 +494,13 @@ export default function Home(
           <section>
             <div className="flex flex-col w-full sm:p-6 items-center justify-center pt-32">
               <div className="flex items-center justify-center pt-8">
-                <span className="text-2xl sm:text-6xl text-text-primary font-gothic text-center">
+                <span className="text-2xl sm:text-5xl text-text-primary font-gothic text-center">
                   Check out our fav
                 </span>
               </div>
               <div
                 ref={notationRef}
-                className="flex items-center justify-center w-full text-3xl sm:text-6xl lg:text-7xl text-text-primary font-gothic text-center py-3 space-x-2 sm:space-x-3"
+                className="flex items-center justify-center w-full text-3xl sm:text-6xl lg:text-7xl text-text-secondary font-gothic text-center py-3 space-x-2 sm:space-x-3"
               >
                 <span className="w-fit mr-3">all</span>
                 <RoughNotation
@@ -496,47 +517,79 @@ export default function Home(
                 </RoughNotation>
                 <span>picks.</span>
               </div>
-              <div className="flex flex-wrap w-full h-fit items-center justify-center sm:justify-evenly pt-10">
-                {allNaturalProducts?.slice(0, 6).map((product) => {
-                  const productImage = productsData?.find(
-                    (p) =>
-                      p.type === "IMAGE" &&
-                      product?.itemData?.imageIds?.includes(p.id)
-                  );
-                  const productCategory = categoriesData?.find(
-                    (category) => category.id === product?.itemData?.categoryId
-                  );
-                  return (
-                    <Link
-                      key={product.id}
-                      href={`/shop/${productCategory?.categoryData?.name}/${product.itemData?.name}`}
-                      as={`/shop/${
-                        productCategory?.categoryData?.name
-                      }/${slugify(product.itemData?.name as string)}`}
-                      className="relative overflow-hidden"
-                    >
-                      <div className="flex flex-col justify-center items-center h-32 w-32 m-4 md:m-4 cursor-pointer hover:scale-105">
-                        <div className="relative h-full w-full">
-                          <Image
-                            priority={true}
-                            src={
-                              productImage?.imageData?.url ||
-                              "https://thecheekcomedia.s3.ap-southeast-2.amazonaws.com/placeholder-image.png"
-                            }
-                            width={150}
-                            height={150}
-                            objectFit="cover"
-                            layout="responsive"
-                            className="rounded-md"
-                          />
+              <div className="flex flex-wrap w-full h-fit items-center justify-center pt-10">
+                {randomAllNatural &&
+                  randomAllNatural.map((product) => {
+                    const productImage = productsData?.find(
+                      (p) =>
+                        p.type === "IMAGE" &&
+                        product?.itemData?.imageIds?.includes(p.id)
+                    );
+                    const productCategory = categoriesData?.find(
+                      (category) =>
+                        category.id === product?.itemData?.categoryId
+                    );
+                    const productReview = productReviewsData?.find(
+                      (review) => review.productId === product?.id
+                    );
+                    return (
+                      <div className="flex flex-col items-center justify-center w-fit">
+                        <Link
+                          key={product.id}
+                          href={`/shop/${productCategory?.categoryData?.name}/${product.itemData?.name}`}
+                          as={`/shop/${slugify(
+                            productCategory?.categoryData?.name as string
+                          )}/${slugify(product.itemData?.name as string)}`}
+                          className="relative overflow-hidden"
+                        >
+                          <div className="flex flex-col justify-center items-center h-32 w-32 m-4 md:m-4 cursor-pointer hover:scale-105">
+                            <div className="relative h-full w-full">
+                              <Image
+                                priority={true}
+                                src={
+                                  productImage?.imageData?.url ||
+                                  "https://thecheekcomedia.s3.ap-southeast-2.amazonaws.com/placeholder-image.png"
+                                }
+                                width={150}
+                                height={150}
+                                objectFit="cover"
+                                layout="responsive"
+                                className="rounded-md"
+                              />
+                            </div>
+                            <span className="text-xs sm:text-sm  text-text-primary w-full text-left py-2">
+                              {product?.itemData?.name}
+                            </span>
+                          </div>
+                        </Link>
+                        <div className="w-full flex items-center justify-start pl-3 pt-2">
+                          <Stars review={productReview} />
                         </div>
-                        <span className="text-xs font-bold whitespace-nowrap text-text-primary w-full text-left py-2">
-                          {product?.itemData?.name}
-                        </span>
+                        <div className="w-full flex items-center justify-start pl-3 pt-2">
+                          <span className="text-text-primary text-xs sm:text-sm">
+                            $
+                            {(
+                              Number(
+                                product.itemData?.variations?.[0]
+                                  .itemVariationData?.priceMoney?.amount
+                              ) / 100
+                            ).toFixed(2)}
+                          </span>
+                        </div>
+                        <div className="mt-2">
+                          <button
+                            onClick={() => handleAdd(product)}
+                            className="relative flex w-fit mx-auto bg-button rounded-2xl py-2 px-8 items-center justify-center text-sm font-medium text-white border border-invisible hover:border-black uppercase cursor-pointer"
+                          >
+                            Add to cart
+                            <span className="sr-only">
+                              {product.itemData?.name}
+                            </span>
+                          </button>
+                        </div>
                       </div>
-                    </Link>
-                  );
-                })}
+                    );
+                  })}
               </div>
             </div>
           </section>
