23adbd9e164907ccd4845ec7dda2faed66db0928
sir-mr-bean <kroucher.1019@gmail.com>
2022-07-02 12:35:09 +1000

    test adding strict csrp

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,44 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import * as gtag from "lib/gtag";
+import { NextStrictCSP } from "next-strict-csp";
+import Script from "next/script";
+
+const GTMJs = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', '${gtag.GA_TRACKING_ID}', {
+    page_path: window.location.pathname,
+  });
+`;
+
+NextStrictCSP.inlineJs = [GTMJs];
+
+const HeadCSP = process.env.NODE_ENV === "production" ? NextStrictCSP : Head;
 
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <script
+      <HeadCSP>
+        {process.env.NODE_ENV === "production" && (
+          <meta httpEquiv="Content-Security-Policy" />
+        )}
+        {/* <Script
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        /> */}
+        {/* Google Tag Manager */}
+        {/* {process.env.NODE_ENV === "production" && ( */}
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: GTMJs,
+          }}
         />
-        <script
+        {/* )} */}
+        {/* End Google Tag Manager */}
+        {/* <script
           dangerouslySetInnerHTML={{
             __html: `
             window.dataLayer = window.dataLayer || [];
@@ -19,7 +49,7 @@ export default function Document() {
             });
           `,
           }}
-        />
+        /> */}
         <link
           href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;500;600;700&family=Gothic+A1:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
@@ -40,8 +70,15 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap"
           rel="stylesheet"
         />
-      </Head>
+      </HeadCSP>
       <body>
+        {process.env.NODE_ENV === "production" && (
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src=https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
+        )}
         <Main />
         <NextScript />
       </body>
