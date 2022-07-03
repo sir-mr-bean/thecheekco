2c53bb8db40f4e2767f2026b41ef11b6e43d3e29
sir-mr-bean <kroucher.1019@gmail.com>
2022-07-04 07:45:04 +1000

    fix category links

--- components/Homepage/Banner/BannerCategory.tsx
+++ components/Homepage/Banner/BannerCategory.tsx
@@ -1,5 +1,6 @@
 import Image, { StaticImageData } from "next/image";
 import { slugify } from "@/utils/hooks/useSlugify";
+import Link from "next/link";
 
 const BannerCategory = ({
   category,
@@ -8,26 +9,31 @@ const BannerCategory = ({
   category: string;
   image: StaticImageData;
 }) => {
-  console.log(category);
   return (
     <div className="flex flex-col w-full items-center justify-center">
-      <div className="relative w-28 h-28 lg:w-52 lg:h-52 border rounded-lg border-text-secondary cursor-pointer overflow-hidden">
-        <Image
-          src={image}
-          alt={`Shop ${category}`}
-          objectFit="cover"
-          objectPosition={
-            category === "Home" || category === "Accessories"
-              ? `25% 10%`
-              : `90% 60%`
-          }
-          layout="responsive"
-          height={category === "Home" || category === "Accessories" ? 900 : 600}
-          width={600}
-          priority
-          className="w-full h-full rounded-md"
-        />
-      </div>
+      <Link passHref href={`/shop/${slugify(category)}`}>
+        <a className="text-text-primary">
+          <div className="relative w-28 h-28 lg:w-52 lg:h-52 border rounded-lg border-text-secondary cursor-pointer overflow-hidden">
+            <Image
+              src={image}
+              alt={`Shop ${category}`}
+              objectFit="cover"
+              objectPosition={
+                category === "Home" || category === "Accessories"
+                  ? `25% 10%`
+                  : `90% 60%`
+              }
+              layout="responsive"
+              height={
+                category === "Home" || category === "Accessories" ? 900 : 600
+              }
+              width={600}
+              priority
+              className="w-full h-full rounded-md"
+            />
+          </div>
+        </a>
+      </Link>
       <a
         href={`/shop/${slugify(category)}`}
         className="w-fit px-1.5 sm:px-4 py-1 sm:py-1.5 bg-button text-xs lg:text-sm rounded-xl font-semibold text-white uppercase mt-2 cursor-pointer border border-transparent hover:border-white"
