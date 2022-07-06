0fc601a3246dd593ccfba095b3a0b6802d00cb49
sir-mr-bean <kroucher.1019@gmail.com>
2022-07-06 18:24:05 +1000

    fix _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,18 +23,18 @@ import { NextPageContext } from "next";
 import "@fontsource/gothic-a1";
 import { NextResponse } from "next/server";
 
-export function reportWebVitals(metric: NextWebVitalsMetric) {
-  const { id, name, label, value } = metric;
-  // Use `window.gtag` if you initialized Google Analytics as this example:
-  // https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics/pages/_app.js
-  window.gtag("event", name, {
-    event_category:
-      label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
-    value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
-    event_label: id, // id unique to current page load
-    non_interaction: true, // avoids affecting bounce rate.
-  });
-}
+// export function reportWebVitals(metric: NextWebVitalsMetric) {
+//   const { id, name, label, value } = metric;
+//   // Use `window.gtag` if you initialized Google Analytics as this example:
+//   // https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics/pages/_app.js
+//   window.gtag("event", name, {
+//     event_category:
+//       label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
+//     value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
+//     event_label: id, // id unique to current page load
+//     non_interaction: true, // avoids affecting bounce rate.
+//   });
+// }
 
 const MyApp = ({
   Component,
