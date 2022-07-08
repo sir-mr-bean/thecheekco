996f5f5587703ecc586dea6beacbf4879036b426
sir-mr-bean <kroucher.1019@gmail.com>
2022-07-08 16:09:23 +1000

    subscription checkout half complete

--- components/CheekyBox/Pages/FinalPage.tsx
+++ components/CheekyBox/Pages/FinalPage.tsx
@@ -6,6 +6,11 @@ import {
   PageTwoOptions,
 } from "@/types/PageOptions";
 import { AiOutlineCheck, AiOutlineClose } from "react-icons/ai";
+import {
+  CreditCard,
+  GooglePay,
+  PaymentForm,
+} from "react-square-web-payments-sdk";
 import NoThanks from "./FinalPage/NoThanks";
 import YesPlease from "./FinalPage/YesPlease";
 
@@ -25,111 +30,109 @@ const FinalPage = ({
   gift: boolean;
 }) => {
   return (
-    <div className="w-full justify-center">
-      <div className="mx-4 rounded-lg bg-bg-tan p-2">
-        <h1 className="pb-6 text-2xl">Review your Cheeky Box:</h1>
-        <div className="flex w-full flex-wrap items-start justify-around">
-          <span className="rounded-lg border border-text-secondary p-2 text-center text-sm sm:text-lg">
-            {pageOneOptions.bath
-              ? "Just Bath Products"
-              : pageOneOptions.shower
-              ? "Just Shower Products"
-              : "Both Shower and Bath Products"}
-          </span>
-          <div className="flex flex-col items-start justify-center rounded-lg border border-text-secondary p-2 text-start text-sm sm:text-lg">
-            <ul>
-              <li>
-                {pageTwoOptions.bathSoak ? (
-                  <YesPlease item="Bath Soaks" />
-                ) : (
-                  <NoThanks item="Bath Soaks" />
-                )}
-              </li>
-              <li>
-                {pageTwoOptions.bodyButter ? (
-                  <YesPlease item="Body Butter" />
-                ) : (
-                  <NoThanks item="Body Butter" />
-                )}
-              </li>
-              <li>
-                {pageTwoOptions.bodyWash ? (
-                  <YesPlease item="Body Wash" />
-                ) : (
-                  <NoThanks item="Body Wash" />
-                )}
-              </li>
-              {/* <li>
-                {pageTwoOptions.bubbleBath ? (
-                  <YesPlease item="Bubble Bath" />
-                ) : (
-                  <NoThanks item="Bubble Bath" />
-                )}
-              </li> */}
-              <li>
-                {pageTwoOptions.conditioner ? (
-                  <YesPlease item="Conditioner" />
-                ) : (
-                  <NoThanks item="Conditioner" />
-                )}
-              </li>
-              <li>
-                {pageTwoOptions.shampooBar ? (
-                  <YesPlease item="Shampoo Bar" />
-                ) : (
-                  <NoThanks item="Shampoo Bar" />
-                )}
-              </li>
-              {/* <li>
-                {pageTwoOptions.showerSteamer ? (
-                  <YesPlease item="Shower Steamer" />
-                ) : (
-                  <NoThanks item="Shower Steamer" />
-                )}
-              </li> */}
-              <li>
-                {pageTwoOptions.soapBar ? (
-                  <YesPlease item="Soap Bar" />
-                ) : (
-                  <NoThanks item="Soap Bar" />
-                )}
-              </li>
-            </ul>
-          </div>
-          <div className="flex flex-col items-start justify-center rounded-lg border border-text-secondary p-2 text-start text-sm sm:text-lg">
-            <ul>
-              <li>
-                {pageThreeOptions.sweet ? (
-                  <YesPlease item="Sweet" />
-                ) : (
-                  <NoThanks item="Sweet" />
-                )}
-              </li>
-              <li>
-                {pageThreeOptions.fruity ? (
-                  <YesPlease item="Fruity" />
-                ) : (
-                  <NoThanks item="Fruity" />
-                )}
-              </li>
-              <li>
-                {pageThreeOptions.floral ? (
-                  <YesPlease item="Floral" />
-                ) : (
-                  <NoThanks item="Floral" />
-                )}
-              </li>
-              <li>
-                {pageThreeOptions.fragranceFree ? (
-                  <YesPlease item="Fragrance Free" />
-                ) : (
-                  <NoThanks item="Fragrance Free" />
-                )}
-              </li>
-            </ul>
+    <div className="font-gothica grid w-full grid-cols-1 justify-center rounded-xl bg-white py-4 sm:grid-cols-2">
+      <div className="order-last sm:order-first">
+        <div className="mx-4 p-2">
+          <h1 className="border border-x-transparent border-t-transparent border-b-text-secondary pb-6 text-2xl text-text-secondary">
+            Subscription Box Checkout
+          </h1>
+        </div>
+        <div className="flex h-24 w-full pl-4">
+          <div className="w-full pt-8 text-base">
+            <span className="text-text-secondary">
+              Thanks for choosing cheeky! We cannot wait to ship your goodies
+            </span>
           </div>
         </div>
+        <div className="flex w-full flex-col items-start justify-center space-y-3 pl-4">
+          <span className="w-full pl-4 text-text-secondary">
+            Payment Method
+          </span>
+          <div className="w-fit rounded-lg bg-button p-8">
+            <PaymentForm
+              applicationId={
+                process.env.NEXT_PUBLIC_SQUARE_APP_ID
+                  ? process.env.NEXT_PUBLIC_SQUARE_APP_ID
+                  : ""
+              }
+              locationId={
+                process.env.NEXT_PUBLIC_SQUARE_LOCATION_ID
+                  ? process.env.NEXT_PUBLIC_SQUARE_LOCATION_ID
+                  : ""
+              }
+              createPaymentRequest={() => ({
+                countryCode: "AU",
+                currencyCode: "AUD",
+                displayItems: [
+                  {
+                    amount: "22.15",
+                    label: "Item to be purchased",
+                    id: "SKU-12345",
+                    imageUrl: "https://url-cdn.com/123ABC",
+                    pending: true,
+                    productUrl: "https://my-company.com/product-123ABC",
+                  },
+                ],
+                taxLineItems: [
+                  {
+                    label: "State Tax",
+                    amount: "8.95",
+                    pending: true,
+                  },
+                ],
+                requestBillingContact: false,
+                requestShippingContact: false,
+                total: {
+                  amount: "100",
+                  label: "Total",
+                },
+              })}
+              cardTokenizeResponseReceived={async (token, buyer) => {}}
+            >
+              <CreditCard
+                includeInputLabels
+                buttonProps={{
+                  isLoading: false,
+                  css: {
+                    backgroundColor: "#a75e2f",
+                    fontSize: "14px",
+                    color: "#fff",
+                    "&:hover": {
+                      backgroundColor: "#E3BB9D",
+                    },
+                  },
+                }}
+              >
+                <div className="flex h-full w-full items-center justify-center">
+                  update this
+                  {/* {orderProcessing ? (
+                                            <div className="flex w-full items-center justify-center space-x-2">
+                                              <span>Processing Order</span>
+                                              <BeatLoader
+                                                size={8}
+                                                color="#602d0d"
+                                              />
+                                            </div>
+                                          ) : (
+                                            <span>Pay ${total.toFixed(2)}</span>
+                                          )} */}
+                </div>
+              </CreditCard>
+              <div className="py-4">
+                <GooglePay buttonColor="white" />
+              </div>
+            </PaymentForm>
+          </div>
+        </div>
+        <div className="pl-4">
+          <span className="text-xs text-text-secondary">
+            Subscription boxes will be posted on the 7th of each new month,
+            please allow up to two weeks for shipping. Subscriptions can be
+            paused for up to 2 months and cancelled at any time*
+          </span>
+        </div>
       </div>
+      <span>hi!</span>
     </div>
   );
 };
