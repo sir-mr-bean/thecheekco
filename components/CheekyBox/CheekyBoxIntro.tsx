f64198306ea469b2585ae1917fc6ee66e253e73f
sir-mr-bean <kroucher.1019@gmail.com>
2022-07-08 10:46:59 +1000

    cheeky box first page complete

--- components/CheekyBox/CheekyBoxIntro.tsx
+++ components/CheekyBox/CheekyBoxIntro.tsx
@@ -2,27 +2,27 @@ import SubscriptionOption from "./Pages/CheekyBoxForm/Intro/SubscriptionOption";
 
 const CheekyBoxIntro = ({ nextStep }: { nextStep: () => void }) => {
   return (
-    <div className="flex flex-col items-center text-sm sm:text-xl justify-center font-gothic w-full">
-      <div className="flex flex-col items-center w-full px-5 lg:px-20 space-y-10">
-        <span className="text-2xl sm:text-4xl mt-3 text-center">
+    <div className="flex w-full flex-col items-center justify-center font-gothic text-sm sm:text-xl">
+      <div className="flex w-full flex-col items-center space-y-10 px-5 lg:px-20">
+        <span className="mt-3 text-center text-2xl sm:text-4xl">
           monthly subscription
         </span>
-        <div className="my-3 flex flex-col w-fit items-center justify-center text-center">
+        <div className="my-1 flex w-fit flex-col items-center justify-center text-center sm:my-3">
           <span className="text-base">
             The Cheeky Box is a decadent collection of limited edition & store
             favs.
           </span>
-          <span className="text-base my-1">
+          <span className="my-1 text-base">
             Designed to expand your bathing experience and top up on bathroom
             necessities.
           </span>
         </div>
-        <div className="flex flex-col w-full sm:flex-row justify-center items-center space-y-3 sm:space-y-0 sm:space-x-24 xl:w-3/4">
-          <SubscriptionOption price="$10" />
-          <SubscriptionOption price="$20" />
+        <div className="flex w-full flex-col items-center justify-center space-y-3 sm:flex-row sm:space-y-0 sm:space-x-24 xl:w-3/4">
+          <SubscriptionOption duration="monthly" price="49.99" />
+          <SubscriptionOption duration="threemonths" price="149.97" />
         </div>
       </div>
-      <div className="w-full flex items-center justify-end my-6">
+      <div className="my-6 flex w-full items-center justify-end">
         <span className="text-text-primary">
           Tell us a bit about your preferences →
         </span>
--- components/CheekyBox/Pages/CheekyBoxForm/Intro/SubscriptionOption.tsx
+++ components/CheekyBox/Pages/CheekyBoxForm/Intro/SubscriptionOption.tsx
@@ -1,9 +1,80 @@
-const SubscriptionOption = ({ price }: { price: string }) => {
+const SubscrinOn = ({
+  price,
+  duration,
+}: {
+  price: string;
+  duration: string;
+}) => {
   return (
-    <div className="w-full h-[360px]  sm:h-[420px] rounded-xl bg-button shadow-sm max-w-xs">
-      <span>{price}</span>
+    <div className="group h-[370px]  w-full max-w-xs rounded-xl bg-button shadow-sm sm:h-[460px] ">
+      <div className="flex h-full w-full flex-col items-center justify-between p-2 sm:p-6">
+        <span className=" text-center text-2xl text-text-primary sm:text-4xl">
+          the cheeky box.
+        </span>
+        {duration === "monthly" ? (
+          <span className="text-base text-text-primary sm:text-lg">
+            monthly subscrin box
+          </span>
+        ) : (
+          <span className="text-base text-text-primary sm:text-lg">
+            3 month gift addition
+          </span>
+        )}
+        <div className="flex w-full flex-col items-center justify-center">
+          <span className=" text-2xl text-text-primary sm:text-3xl">
+            {price}
+          </span>
+          {duration === "monthly" ? (
+            <span className="text-xs text-text-primary">
+              (billed monthly, postage included)
+            </span>
+          ) : (
+            <span className="text-xs text-text-primary">
+              (one off payment, postage included)
+            </span>
+          )}
+        </div>
+        {duration === "monthly" ? (
+          <div className="flex flex-col items-center justify-center text-center text-xs text-text-primary sm:text-sm">
+            <span className="">
+              This little box of luxury a collection of locally handmade,
+              plastic free and fabulous products.
+            </span>
+            <span className="">
+              Each month will sport a selection of soap, skin care, shower & or
+              bath, & accessories.
+            </span>
+          </div>
+        ) : (
+          <div className="flex flex-col items-center justify-center text-center text-xs text-text-primary sm:text-sm">
+            <span className="">
+              The gift that keeps on giving, pamper a loved one with care &
+              convenience. This box will arrive at the beginning of the month
+              for 3 months. No fuss, just beautiful handmade products at their
+              door.
+            </span>
+          </div>
+        )}
+        <span className="pb-1 text-center text-xs text-text-primary">
+          Subscription boxes will be shipped at the beginning of the next
+          calender month.
+        </span>
+        {duration === "monthly" ? (
+          <div className="mt-1 flex w-full cursor-pointer items-center justify-center rounded-xl border border-transparent bg-bg-tan py-3 hover:border-white">
+            <span className="text-base font-semibold text-text-primary">
+              Select
+            </span>
+          </div>
+        ) : (
+          <div className="mt-2 flex w-full cursor-pointer items-center justify-center rounded-xl border border-transparent bg-bg-tan py-3 hover:border-white">
+            <span className="text-base font-semibold text-text-primary">
+              Send a gift
+            </span>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
 
-export default SubscriptionOption;
+export default SubscrinOn;
