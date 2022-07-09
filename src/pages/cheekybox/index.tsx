248b4047847a1f29c95a410b6aba7e0df6891b90
sir-mr-bean <kroucher.1019@gmail.com>
2022-07-09 12:36:54 +1000

    add head to cheekybox

--- src/pages/cheekybox/index.tsx
+++ src/pages/cheekybox/index.tsx
@@ -1,10 +1,19 @@
 import CheekyBoxWrapper from "@/components/CheekyBox/CheekyBoxWrapper";
+import Head from "next/head";
 
 const index = () => {
   return (
-    <div>
+    <>
+      <Head>
+        <title>The Cheek Co. - Cheeky Box</title>
+        <meta
+          name="description"
+          content="More than just amazing bath and skin care products. Ethically sourced handmade in Australia, cruelty free, vegan."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <CheekyBoxWrapper />
-    </div>
+    </>
   );
 };
 
